c2f25eea86ad148e6c3a05243c119e9c385d0537
Moquo <mail@moquo.de>
2018-04-30 20:32:41 +0200

    Added url model

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# A shorten url
+class Url(models.Model):
+    url_id = models.CharField("The short url id", max_length=8)
+    orig_url = models.TextField("Original url")
+    created_at = models.DateTimeField("Created at")
+    clicks = models.IntegerField(default=0)
