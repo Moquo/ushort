2d13db47c127885dd032b332fd50ab1b9c711e81
Moquo <mail@moquo.de>
2018-05-01 18:28:19 +0200

    Added UrlAdmin model

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Url
 
-admin.site.register(Url)
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ("url_id", "orig_url", "created_at", "clicks")
+
+admin.site.register(Url, UrlAdmin)
